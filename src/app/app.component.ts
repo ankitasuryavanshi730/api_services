0ad2125f75d42fa14f99c95ef4bbf93027f4ed72
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-03-26 14:14:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'api_services';
+export class AppComponent implements OnInit{
+
+  poduct:any;
+
+
+  constructor(private api:ApiService){
+
+  }
+
+
+  ngOnInit(): void {
+   this.api.get("https://fakestoreapi.com/products").subscribe((result:any)=>{
+
+   this.poduct = result;
+
+   })
+
+
+  }
 }
